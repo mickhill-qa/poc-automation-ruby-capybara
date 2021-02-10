eb1637d35ab01d3553a334c9322f7dded8c8c9d2
Mick Hill - MacBook Pro <mickhill@hotmail.com.br>
2021-02-10 18:16:08 -0300

    [Create] Working with hooks

---
+++ test/features/hooks/hooks.rb
@@ -0,0 +1,18 @@
+### Global ###
+# Before do
+#     log 'Inicio de todos os Cenarios.'
+# end
+
+# After do
+#     log 'Fim de todos os Cenarios.'
+# end
+
+
+### Tag ###
+# Before '@pesquisaUndefined' do
+#     log 'Apenas no INICIO desse Cenarios'
+# end
+
+# After '@pesquisaUndefined' do
+#     log 'Apenas no FIM desse Cenarios'
+# end

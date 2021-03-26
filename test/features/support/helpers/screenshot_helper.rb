f5163347d1b7d208773f055fd0491f1391be1493
Mick Hill <mickhill@hotmail.com.br>
2021-03-26 01:17:16 -0300

    Corrigindo erro no ci relacionado ao screenshot

--- test/features/support/helpers/screenshot_helper.rb
+++ test/features/support/helpers/screenshot_helper.rb
@@ -4,17 +4,18 @@ module ScreenshotHelper
     data                = Time.now.strftime('%Y-%m-%d').to_s + "/"
     foto                = "#{data_e_hora}_-_#{SELECTED_ENV}_#{SELECTED_BROWSER}_ERRO_#{name_step_erro}.png"
     caminho_screenshot  = SCREENSHOT_PATH + data + foto
-    erro_scree          = page.save_screenshot(caminho_screenshot)
-    shot                = Base64.encode64(File.open(erro_scree, "rb").read)
-    attach(shot, 'image/png')
+    screen_saved        = page.save_screenshot(caminho_screenshot, full: true)
+    img_base64          = Base64.encode64(File.open(screen_saved, "r:UTF-8", &:read))
+    attach(img_base64, 'image/png;base64')
   end
-  
+
   def screenshot
     data_e_hora         = Time.now.strftime('%Y-%m-%d_-_%H-%M-%S-%L').to_s
     data                = Time.now.strftime('%Y-%m-%d').to_s + "/"
     foto                = "#{data_e_hora}_-_#{SELECTED_ENV}_#{SELECTED_BROWSER}.png"
     caminho_screenshot  = SCREENSHOT_PATH + data + foto
-    page.save_screenshot(caminho_screenshot, full: true)
-    attach(caminho_screenshot, 'image/png')
+    screen_saved        = page.save_screenshot(caminho_screenshot, full: true)
+    img_base64          = Base64.encode64(File.open(screen_saved, "r:UTF-8", &:read))
+    attach(img_base64, 'image/png;base64')
   end
 end
